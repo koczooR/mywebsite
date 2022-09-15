2e4fe85dcee96ddca932fb447f7d520c3cb66f44
koczooR <koczorowski94@gmail.com>
2022-09-15 20:47:34 +0200

    add demo button to card, rwd card

--- src/components/Home/Portfolio/Card.js
+++ src/components/Home/Portfolio/Card.js
@@ -1,10 +1,13 @@
-export const Card = ({ img, title, text }) => {
+export const Card = ({ img, title, text, url }) => {
   return (
     <div className="card">
       <div className="bg_container" style={{ backgroundImage: `url(${img})` }}></div>
       <div className="text_container">
         <p className="title">{title}</p>
         <p className="description">{text}</p>
+        <a href={url} className="card_btn">
+          DEMO
+        </a>
       </div>
     </div>
   );
--- src/components/Home/Portfolio/Portfolio.js
+++ src/components/Home/Portfolio/Portfolio.js
@@ -16,18 +16,21 @@ export const Portfolio = () => {
       img: pokemon,
       title: "Final project of the javaScript Developer course",
       text: "A website based on a Pokemon game with some interactions. The project aims at the purpose of showing the acquired skills during the course. The project was created with React.js and SASS.",
+      url: "https://earnest-starship-84e462.netlify.app",
     },
     {
       id: 2,
       img: portfoliolab,
       title: "Oddam w dobre ręce",
       text: `The project "Oddam w dobre ręce" was prepared in order to expand my skills. It is a website with a simple form, which is to make it easier to donate unnecessary items to trusted institutions. This project uses React.js, SASS and Firebase.`,
+      url: "https://chipper-bublanina-9da9cd.netlify.app/",
     },
     {
       id: 3,
       img: space,
       title: "Space tourism",
       text: `The project "Space Tourism" was made in order to expand my skills. It is a responsive multi-page website. The project was taken from www.frontendmentor.io. This project uses React.js and SASS.`,
+      url: "https://silver-bavarois-eb68ef.netlify.app",
     },
   ];
 
@@ -62,7 +65,7 @@ export const Portfolio = () => {
             (item) =>
               currentCard === item.id && (
                 <CSSTransition key={item.id} timeout={1000} classNames="item">
-                  <Card key={item.id} img={item.img} title={item.title} text={item.text} />
+                  <Card key={item.id} img={item.img} title={item.title} text={item.text} url={item.url} />
                 </CSSTransition>
               )
           )}
